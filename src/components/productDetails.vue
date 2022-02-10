970ff7fb846397d780ef42db1f0f1ddbbdda2bc7
root <mlingam3499@conestogac.on.ca>
2022-02-10 00:59:34 -0500

    Mohan : Finished Product Details with comments and cart

--- src/components/productDetails.vue
+++ src/components/productDetails.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="ba bw2 b--orange br3" style="margin: 2rem">
     <div v-for="product in products" :key="product.id" class="dib">
+        
       <article v-if="product.id == selectedProductId" class="center">
         <div class="mw9 center bg-light-yellow">
           <div class="cf">
@@ -23,17 +24,38 @@
                   <h4 class="f5">Seller :{{ product.seller }}</h4>
                 </div>
 
-                <div></div>
-
                 <button
                   class="bg-hot-pink white bn pa2 w-70 br2 f7 fw2 mv2 pointer"
-                  @click="addProductToCart(product)"
-                >
+                  @click="addProductToCart(product)">
                   Add to cart
                 </button>
+                <div v-for="cartItem in cartItems" :key="cartItem" class="dib">
+                <div v-if="cartItem.id == selectedProductId" >
+                <h1>Cart Item:</h1> <input
+                  v-model.number="cartItem.quantity"
+                  min="1"
+                  :max="product.newQuantityInStock"
+                  type="number"
+                  id="quantity"
+                  class="form-control w-75 d-block"
+                />
+              </div>
+              </div>
+
               </div>
             </div>
           </div>
+        </div>
+        <div class="flex flex-column items-start">
+        <h1 class="fl">Reviews</h1>
+        <div class="flex flex-wrap"> 
+        <div class="outline w-49 pa3 mr2 ml2 mt2 mb2" v-for="review in product.comments" :key="review">
+          <h3 class="f4">{{ review.name }}</h3>
+          <p class="">Rating {{review.rating}}/5</p>
+          <p class="">{{review.comment}}</p>
+          <h4 class="f6">Date :- {{ review.date }}</h4>
+         </div>
+        </div>
         </div>
       </article>
     </div>
@@ -49,6 +71,9 @@ export default {
     selectedProductId() {
       return this.$store.state.selectedProductId;
     },
+    cartItems(){
+      return this.$store.state.cart;
+    }
   },
   methods: {
     addProductToCart(product) {
